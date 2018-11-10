038b69583ca2258c819acb726d921848d6e404c2
대엽 여 <21300443@handong.edu>
2018-11-10 15:48:50 +0900

    main

--- lib/global/currentUser.dart
+++ lib/global/currentUser.dart
@@ -1,68 +1,17 @@
 library my_prj.globals;
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class CurrentUser {
-  
-  String _displayName;
-  String _email;
-  DateTime _finalLogin;
-  String _photoUrl;
-  String _uid;
-  String _level;
-  String _classof;
-  String _phoneNumber;
-  DocumentSnapshot db;
   final GoogleSignIn _googleSignIn = new GoogleSignIn();
+  FirebaseUser user;
 
-  String getDisplayName(){return _displayName;}
-  String getEmail(){return _email;}
-  DateTime getFinalLogin(){return _finalLogin;}
-  String getphotoUrl(){return _photoUrl;}
-  String getUid(){return _uid;}
-  String getLevel(){return _level;}
-  String getClass(){
-    if(_classof != null || _classof == "") return _classof;
-    else return "학번을 등록해 주세요";
+  setCurrentUser(FirebaseUser input){
+    this.user = input;
   }
-  String getPhoneNumber(){
-    if(_phoneNumber != null || _phoneNumber == "") return _phoneNumber;
-    else return "전화번호를 등록해 주세요";  
+  clear(){
+    this.user = null;
   }
-
-  void setCurrentUser(
-    displayName,
-    email,
-    finalLogin,
-    photoUrl,
-    uid
-  )async{
-    this._displayName = displayName;
-    this._email = email;
-    this._finalLogin = finalLogin;
-    this._photoUrl = photoUrl;
-    this._uid = uid;
-    db = await Firestore.instance.collection('club').document('슬기짜기').collection('users').document(_uid).get();
-    if(db.exists){
-      setlevel();
-      _init();
-    }
-    else {
-      _level = "guest";
-    }
-  }
-
-  void clear(){
-    _displayName = null;
-    _email = null;
-    _finalLogin = null;
-    _photoUrl = null;
-    _uid = null;
-    _classof = null;
-    _phoneNumber = null;
-    db = null;
-  }
-
   googleLogIn(){
     _googleSignIn.signIn();
   }
@@ -72,27 +21,6 @@ class CurrentUser {
   }
   GoogleSignIn getGoogleLogIn(){
     return _googleSignIn;
-  }
-
-  void setlevel() {
-    _level = db.data['level'];
-  }
-  void setlevelDirect(String input) {
-    _level = input;
-  }
-  void _init(){
-    var classof = db.data['classof'];
-    var phoneNumber = db.data['phoneNumber'];
-    if(classof != null) _classof = classof;
-    else _classof = "";
-    if(phoneNumber != null)_phoneNumber = phoneNumber;
-    else _phoneNumber = "";
-  }
-  void setClass(String input){
-    _classof = input;
-  }
-  void setPhoneNumber(String input){
-    _phoneNumber = input;
   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import './global/currentUser.dart' as cu;
+
+import 'home.dart';
+
 void main() => runApp(RouteApp());
 
 class RouteApp extends StatefulWidget {
@@ -12,15 +20,29 @@ class RouteState extends State<RouteApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Main',
-      home: LoginPage(),
+      home: HomePage(),
       theme: ThemeData(
         primaryColor: Colors.white,
         accentColor: Colors.black,
       ),
       initialRoute: '/login',
+      onGenerateRoute: _getRoute,
       routes:{
+        '/home': (context) => HomePage(),
         '/login': (context) => LoginPage(),
       },
+    );
+  }
+
+  Route<dynamic> _getRoute(RouteSettings settings) {
+    if (settings.name != '/login') {
+      return null;
+    }
+
+    return MaterialPageRoute<void>(
+      settings: settings,
+      builder: (BuildContext context) => LoginPage(),
+      fullscreenDialog: true,
     );
   }
 }
@@ -32,13 +54,64 @@ class LoginPage extends StatefulWidget{
 
 class LoginState extends State<LoginPage>{
 
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  
+  Future<FirebaseUser> _googleSignIn()  async {
+    GoogleSignInAccount googleSignInAccount = await cu.currentUser.getGoogleLogIn().signIn().catchError((e)=>print(e));
+    GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+    
+    FirebaseUser user = await _auth.signInWithGoogle(
+      idToken: googleSignInAuthentication.idToken,
+      accessToken: googleSignInAuthentication.accessToken
+    );
+    // print(user);
+    await Firestore.instance.collection('users').document(user.uid).setData({
+      "uid":user.uid,
+      "displayName": user.displayName,
+      "photoUrl": user.photoUrl,
+      "email": user.email,
+      "phoneNumber":user.phoneNumber,
+    });
+    cu.currentUser.setCurrentUser(user);
+    Navigator.pushNamed(context, '/home');
+    return user;
+  }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
       body: Center(
-        child: Text("hi"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            //logo
+            Container(
+              height: MediaQuery.of(context).size.height/2,
+              child: Image.asset('assets/diamond.png'),
+            ),
+            //googleLogin
+            RaisedButton(
+              child: Text("google",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: ()=>_googleSignIn(),
+              color: Colors.red,
+            ),            
+            //anonymousLogin
+            RaisedButton(
+              child: Text("Anonymous",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: (){},
+              color: Colors.black
+            ),
+          ],
+        ),
       ),
     );
   }
